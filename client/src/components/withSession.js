ce44cedd9f9852651c21666acc06b11b88d4e5f5
Dkes <Dechawat Kesornchan>
2020-02-11 16:33:05 +0100

    Protect routes with auth

--- client/src/components/withSession.js
+++ client/src/components/withSession.js
@@ -5,11 +5,11 @@ import { GET_CURRENT_USER } from "../queries";
 const withSession = Component => props => (
   <Query query={GET_CURRENT_USER}>
     {({ data, loading, refetch }) => {
-        if(loading) return null;
-        // console.log(data)
-      return <Component {...props} refetch={refetch} session={data}/>;
+      if (loading) return null;
+      console.log(data);
+      return <Component {...props} refetch={refetch} session={data} />;
     }}
   </Query>
 );
 
-export default withSession
+export default withSession;
